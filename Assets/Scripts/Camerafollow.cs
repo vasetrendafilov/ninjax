d44be8d693abb778a47290ef08e1f09168217ffb
vasetrendafilov <vase_trendafilov@hotmail.com>
2021-02-24 18:12:01 +0100

    animation

---
+++ Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 0.125f;
+    public Vector3 ofset;
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + ofset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smooth* Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController2D controler;
+    public Animator animator;
+    public float horizontalmove = 0f;
+    public float runspeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    private bool temp = false;
+
+    void Update()
+    {
+        horizontalmove = Input.GetAxisRaw("Horizontal") * runspeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalmove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            temp = true;
+            StartCoroutine(Parkor());
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Fire2"))
+        {
+            crouch = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        controler.Move(horizontalmove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+    IEnumerator Parkor()
+    {
+        yield return new WaitForSeconds(0.2f);
+        temp = false;
+    }
+}
